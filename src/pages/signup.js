65e840910bd588252dac7dfb2786339defa0a07d
Jai Li <lijw@vcu.edu>
2021-11-24 13:54:57 -0500

    added

---
+++ src/pages/signup.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Signup from '../componets/Signup';
+import ScrollToTop from "../componets/ScrollToTop";
+
+const SignupPage =() =>{
+    return (
+        <>
+            <ScrollToTop />
+            <Signup />
+        </>
+    );
+};
+
+export default SignupPage;
