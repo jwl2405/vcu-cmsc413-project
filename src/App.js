d65a4baa3df29eac7f703830b30178ee24160b9b
Jai Li <lijw@vcu.edu>
2021-11-24 13:51:57 -0500

    AddedStuff

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
 import Home from './pages';
 import SigninPage from './pages/signin'
+import SignupPage from "./pages/signup";
 
 function App() {
-  return (
-    <Router>
-        <Switch>
-            <Route path="/" component={Home} exact />
-            <Route path="/signin" component={SigninPage} exact />
-        </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/" component={Home} exact />
+                <Route path="/signin" component={SigninPage} exact />
+                <Route path="/signup" component={SignupPage} exact />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/componets/Signup/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    Container,
+    Form,
+    FormButton,
+    FormContent,
+    FormH1,
+    FormInput,
+    FormLabel,
+    FormWrap,
+    Icon,
+    Text
+} from "./SignupElements";
+
+const Signup = () => {
+    return (
+        <>
+            <Container>
+                <FormWrap>
+                    <Icon to='/'>KeySafe</Icon>
+                    <FormContent>
+                        <Form action='#'>
+                            <FormH1>Sign up</FormH1>
+                            <FormLabel htmlFor='for'>Enter Full Name</FormLabel>
+                            <FormInput type='name' required />
+                            <FormLabel htmlFor='for'>Enter Email Address</FormLabel>
+                            <FormInput type='email' required />
+                            <FormLabel htmlFor='for'>Verify Email Address</FormLabel>
+                            <FormInput type='email' required />
+                            <FormLabel htmlFor='for'>Enter a password</FormLabel>
+                            <FormInput type='password' required />
+                            <FormButton type='submit'>Continue</FormButton>
+                            <Text>Forgot password</Text>
+                        </Form>
+                    </FormContent>
+                </FormWrap>
+            </Container>
+        </>
+    );
+};
+
+export default Signup;
